ab96b5de61a8c1e98ff04eded3e0403a66ce1f2c
Deividson Callejas <dejacazu@tutanota.com>
2024-12-10 14:44:29 -0500

    Se agrega manejo de errores para mejorar experiencia de usuario

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeServiceService as EmployeeService } from './employee-service.service';
+import swal from 'sweetalert';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
@@ -9,28 +11,60 @@ import { EmployeeServiceService as EmployeeService } from './employee-service.se
 })
 export class EmployeeComponent implements OnInit {
   
+  spinner: boolean = false;
   employees: Employee[] = [];
   selectedEmployee: Employee | undefined;
   searchTerm: string = '';
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(
+    private employeeService: EmployeeService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
     
   }
 
   search(): void {
+    this.spinner = true
     if (this.searchTerm != '') this.getEmployeeDetails(this.searchTerm)
     else this.getEmployees()
   }
 
   getEmployees(): void {
     this.employeeService.getEmployees()
-      .subscribe(employees => this.employees = employees);
+      .subscribe({
+        next: (employees) => {
+          this.employees = employees;
+          this.spinner = false;
+        }, 
+        error: () => this.handleError()
+      });
   }
 
   getEmployeeDetails(id: string): void {
     this.employeeService.getEmployee(id)
-      .subscribe(employee => this.selectedEmployee = employee);
+      .subscribe({
+        next: (employee) => {
+          this.selectedEmployee = employee;
+          this.spinner = false;
+        }, 
+        error: () => this.handleError()
+      });
+  }
+
+  handleError(): void {
+    this.spinner = false;
+    setTimeout(() => {
+      swal({
+        title: "Algo, sucedió",
+        text: "Parece que no es posible obtener los datos del servidor ahora mismo, por favor intente más tarde",
+        icon: "error",
+        // buttons: ["Aww yiss!"],
+      })
+    })
+    
+  
+   
   }
 }
